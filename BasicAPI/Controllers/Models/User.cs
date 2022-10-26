7a6afb5546fa8a18004b4135125cd31ff81758f2
luan.cabral <luan.cabral@NDD-NOT-DEV490>
2022-10-26 19:44:13 -0300

    Adicionar arquivos de projeto.

---
+++ BasicAPI/Controllers/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace BasicAPI.Controllers.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(int Id, string name, string email, string password)
+        {
+            ID = Id;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public User()
+        {
+        }
+    }
+}
---
+++ BasicAPI/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using BasicAPI.Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BasicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        List<User> users = new List<User>
+        {
+            new User(1, "Luan", "luan@ndd.com", "luan123"),
+            new User(2, "Carlos", "carlos@ndd.com", "carlos123"),
+            new User(3, "Lucas", "lucas@ndd.com", "lucas123")
+        };
+
+        // GET: api/user
+        [HttpGet]
+        public List<User> Get()
+        {
+            return users;
+        }
+
+        // GET api/user/2
+        [HttpGet("{id}", Name = "Get")]
+        public User Get(int id)
+        {
+            User user = users.Find(u => u.ID == id);
+            return user;
+        }
+
+        // POST api/user
+        [HttpPost]
+        public List<User> Post([FromBody] User user)
+        {
+            users.Add(user);
+            return users;
+        }
+
+        // PUT api/user/3
+        [HttpPut("{id}")]
+        public List<User> Put(int id, [FromBody] User user)
+        {
+            User userToUpdate = users.Find(u => u.ID == id);
+            int index = users.IndexOf(userToUpdate);
+
+            users[index].Name = user.Name;
+            users[index].Email = user.Email;
+            users[index].Password = user.Password;
+
+            return users;
+        }
+
+        // DELETE api/user/2
+        [HttpDelete("{id}")]
+        public List<User> Delete(int id)
+        {
+            User user = users.Find(u => u.ID == id);
+            users.Remove(user);
+            return users;
+        }
+    }
+}
